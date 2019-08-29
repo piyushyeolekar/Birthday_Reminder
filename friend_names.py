909847dee5bde337ad471cc917673b25167955f0
Piyush Yeolekar <piyushyeolekar30@gmail.com>
2019-08-29 14:40:32 +0530

    Add files via upload

---
+++ friend_names.py
@@ -0,0 +1,9 @@
+#Birthday List of Friends
+#Please enter date in the form of DD/MM/YY without entering any special character
+#between the date
+
+
+Friend_List = [
+            ["Sunny", "27/01/97", 1234567890],
+            ["john", "01/01/19", 9876543210]
+            ]
